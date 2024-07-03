1fa9c5883d43cdd568d93e99e6744a9e021cffd2
wongjoshua2005 <69170386+wongjoshua2005@users.noreply.github.com>
2024-07-03 15:23:40 -0400

    Added more comments and fix the organization of code
    I added in comments to clarify the functionality
    of my calculator and a reflection on what to
    improve after this project.

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Calculator
+
+Reflection: This project definitely did take a lot of time to work on.
+One of my biggest mistakes was not planning enough to include the extra
+credit work because I should have done that. Consequently, I cannot do
+the keyboard support. Overall, the project was very eye opening. I realized
+that I need to work better on planning things out and improve my coding in
+JavaScript. I will definitely be re reading back on how to write clean code,
+and improve my planning next time. I cannot wait to improve my programming
+skills and start making impactful projects to add onto my resume. :)
--- script.js
+++ script.js
@@ -1,30 +1,35 @@
-function operate(num1, num2, operator)
-{
+/**
+ * The operate() method returns the calculations of the two numbers
+ * depending on the operator that the user entered into the calculator.
+ * Useful for doing the calculations when user entered the equal sign.
+ */
+function operate(num1, num2, operator) {
     // Checks the operator user entered to return back the calculation
-    switch (operator)
-    {
+    switch (operator) {
         case "+":
             return num1 + num2;
+
         case "-":
             return num1 - num2;
+
         case "*":
             return num1 * num2;
+
         case "/":
             return num1 / num2;
+
         default:
             console.log("ERROR! INVALID OPERATOR!!!");
             break;
     }
 }
 
-function interactButton(numVal, first_num, second_num, verifySecond,
-     dotUsed, negativeVal)
-{
-    
-}
-
-function display()
-{
+/**
+ * The display() method displays the entire calculations onto the screen
+ * whenever they enter an input using the buttons. Useful for updating the
+ * display whenever a user entered a button or value.
+ */
+function display() {
     // Let program know user entered operator, decimal, or negative value
     let onSecondNum = false, usedDot = false, usedNegative = false;
 
@@ -36,76 +41,58 @@ function display()
     const result = document.querySelector(".results");
     result.textContent = firstNum;
 
-    // Keyboard support
-    document.body.addEventListener("keydown", (e) => {
-        console.log(e.key);
-        switch (e.key)
-        {
-            case "Escape":
-
-                break;
-        }
-    });
-
     const input = document.querySelectorAll(".input");
 
     // Checks through all the buttons to see if any button was clicked
-    input.forEach(function(num) {
+    input.forEach(function (num) {
+
         // To show the user's input on the screen
-        num.addEventListener("click", function() {
+        num.addEventListener("click", function () {
             // To retrieve the button's text that user trying to enter
             const number = num.textContent;
 
-            // Boolean to ensure it's a number and not any other button
-            const validNum = number !== "AC" && number !== "+/-" && number !== "%"
-            && number !== "+" && number !== "-" && number !== "*" 
-            && number !== "/" && number !== "=" && number !== "." 
-            && firstNum.length < 12;
+            // Ensure number when doing calculation and fits within length
+            const notNums = ["AC", "+/-", "%", "+", "-", "*", "/", "=", "."];
+            const checkLength = firstNum.length <= 12 && secondNum.length <= 12;
 
             // Checks if user entered a button to display on screen
-            if (validNum)
-            {
+            if (!notNums.includes(number) && checkLength) {
+                
                 // Runs all operations for the first number use entered
-                if (!onSecondNum)
-                {
+                if (!onSecondNum) {
                     // To add onto the display ensuring no errors in calculation
-                    if (firstNum !== "0")
-                    {
+                    if (firstNum !== "0") {
                         firstNum += number;
                         result.textContent = firstNum;
                     }
-                    else
-                    {
+                    else {
                         firstNum = "";
                         firstNum += number;
                         result.textContent = firstNum;
                     }
                 }
                 // Runs all operations for when user enters second number
-                else
-                {
+                else {
                     // To start new numbers after first numbers are entered
-                    if (secondNum !== firstNum)
-                        {
-                            secondNum += number;
-                            result.textContent = secondNum;
-                        }
-                        else
-                        {
-                            secondNum = "";
-                            secondNum += number;
-                            result.textContent = secondNum;
-                        }                    
+                    if (secondNum !== firstNum) {
+                        secondNum += number;
+                        result.textContent = secondNum;
+                    }
+                    else {
+                        secondNum = "";
+                        secondNum += number;
+                        result.textContent = secondNum;
+                    }
                 }
             }
 
             // Check if the button entered was not a number
-            switch (number)
-            {
+            switch (number) {
                 // Restarts everything
                 case "AC":
                     display();
                     break;
+
                 // Starts the second number input when using operator
                 case "+":
                 case "-":
@@ -117,23 +104,22 @@ function display()
                     result.textContent = secondNum;
                     usedDot = false;
                     break;
+
                 // Calculates the number
                 case "=":
                     // Joke if user divides by zero
-                    if (secondNum === "0")
-                    {
+                    if (secondNum === "0") {
                         firstNum = "Nice try. ;)"
                         result.textContent = firstNum;
                     }
-                    else
-                    {
+                    else {
                         // Converts the string numbers into actual integers
                         let convertNum1 = parseFloat(firstNum);
                         let convertNum2 = parseFloat(secondNum);
                         // Resets the first number to display the result
                         firstNum = "";
-                        firstNum += `${operate(convertNum1, 
-                        convertNum2, operator)}`;
+                        firstNum += `${operate(convertNum1,
+                            convertNum2, operator)}`;
                         result.textContent = firstNum;
                     }
 
@@ -143,54 +129,56 @@ function display()
                     onSecondNum = false;
                     usedDot = false;
                     break;
+
+                // To give the user functionality to calculate with decimals
                 case ".":
-                    if (onSecondNum && !usedDot)
-                    {
+                    // Adds decimal if on the second number after operator
+                    if (onSecondNum && !usedDot) {
                         secondNum += ".";
                         result.textContent = secondNum;
                     }
-                    else if (!usedDot)
-                    {
+                    else if (!usedDot) {
                         firstNum += ".";
                         result.textContent = firstNum;
                     }
                     usedDot = true;
                     break;
+
+                // Allows the user to calculate with negative numbers
                 case "+/-":
-                    if (!usedNegative && onSecondNum)
-                    {
+                    // Verify to add negative num on second value
+                    if (!usedNegative && onSecondNum) {
                         secondNum = `${secondNum}`;
                         result.textContent = secondNum;
                         usedNegative = true;
                     }
-                    else if (usedNegative && onSecondNum)
-                    {
+                    // Removes negative sign if the user wants positive
+                    else if (usedNegative && onSecondNum) {
                         secondNum = secondNum.slice(1);
                         result.textContent = secondNum;
                         usedNegative = false;
                     }
-                    else if (!usedNegative)
-                    {
+                    // Does same thing only for the first number
+                    else if (!usedNegative) {
                         firstNum = `-${firstNum}`;
                         result.textContent = firstNum;
                         usedNegative = true;
                     }
-                    else if (usedNegative)
-                    {
+                    else if (usedNegative) {
                         firstNum = firstNum.slice(1);
                         result.textContent = firstNum;
                         usedNegative = false;
                     }
                     break;
+
+                // Changes to a percentage depending on second val or first val
                 case "%":
-                    if (onSecondNum)
-                    {
+                    if (onSecondNum) {
                         let percentage = parseFloat(secondNum) / 100;
                         secondNum = percentage;
                         result.textContent = secondNum;
                     }
-                    else
-                    {
+                    else {
                         let percent = parseFloat(firstNum) / 100;
                         firstNum = percent;
                         result.textContent = firstNum;
